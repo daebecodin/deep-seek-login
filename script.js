9d9075f5a64965c324a89ee86a64a85af971d112
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-09-22 23:28:20 -0700

    refactor: focus the page on the login form

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const passwordInput = document.querySelector("#password");
 const eyeButton = document.querySelector(".eye-button");
 const loginForm = document.querySelector(".login-form");
-const cookieBanner = document.querySelector(".cookie-banner");
-const cookieButtons = document.querySelectorAll(".cookie-button");
 
 eyeButton.addEventListener("click", () => {
   const passwordIsHidden = passwordInput.type === "password";
@@ -14,10 +12,4 @@ eyeButton.addEventListener("click", () => {
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
-});
-
-cookieButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    cookieBanner.hidden = true;
-  });
 });
